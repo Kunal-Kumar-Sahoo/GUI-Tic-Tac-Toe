84b0de824ab27dfc1a78becc0189e01bc71d4d2e
Kunal Kumar Sahoo <kunal@thinkpad.me>
2021-10-24 19:54:28 +0530

    Completed drawing functions

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ class Tic_Tac_Toe():
         self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
         self.canvas.pack()
         # Input from user in form of clicks
-        # self.window.bind('<Button-1>', self.click)
+        self.window.bind('<Button-1>', self.click)
 
         self.initialize_board()
         self.player_X_turns = True
@@ -48,6 +48,59 @@ class Tic_Tac_Toe():
         self.player_X_starts = not self.player_X_starts
         self.player_X_turns = self.player_X_starts
         self.board_status = np.zeros(shape=(3, 3))
+
+    
+    # Drawing Functions
+
+    def draw_0(self, logical_position):
+        logical_position = np.array(logical_position)
+        grid_position = self.convert_logical_to_grid_position(logical_position)
+        self.canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
+                                grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
+                                outline=symbol_O_color)
+
+
+    def draw_X(self, logical_position):
+        logical_position = np.array(logical_position)
+        grid_position = self.convert_logical_to_grid_position(logical_position)
+        self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
+                                grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
+                                fill=symbol_O_color)
+        self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] + symbol_size,
+                                grid_position[0] + symbol_size, grid_position[1] - symbol_size, width=symbol_thickness,
+                                fill=symbol_O_color)
+
+    def display_gameover(self):
+        if self.X_wins:
+            self.X_score += 1
+            text = "Winner: Player 1 (X)"
+            color = symbol_X_color
+
+        elif self.O_wins:
+            self.O_score += 1
+            text = "Winner: Player 2 (O)"
+            color = symbol_O_color
+        
+        else:
+            self.tie_score += 1
+            text = "It's a tie"
+            color = 'gray'
+        
+        self.canvas.delete("all")
+        self.canvas.create_text(size_of_board / 2, size_of_board / 3, fill=color, text=text)
+
+        score_text = 'Scores\n'
+        self.canvas.create_text(size_of_board / 2, 5* size_of_board / 8, fill=Green_color, text=score_text)
+
+        score_text = 'Player 1 (X): ' + str(self.X_score) + '\n'
+        score_text += 'Player 2 (O): ' + str(self.O_score) + '\n'
+        score_text += 'Tie           ' + str(self.tie_score)
+        self.canvas.create_text(size_of_board / 2, 3 * size_of_board / 4, fill=Green_color, text=score_text)
+
+        self.reset_board = True
+
+        score_text = 'Click to play again\n'
+        self.canvas.create_text(size_of_board / 2, 15 * size_of_board / 16, fill='gray', text=score_text)
 
 
     
